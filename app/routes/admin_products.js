d71a702fec0c6d98e34d5bf66d19606b9b22d4ed
Eduardo <hurtado.josegomez@gmail.com>
2023-10-22 16:19:22 -0600

    HTTP testing finished. Pending: queries for GET product (1st test)

--- app/routes/admin_products.js
+++ app/routes/admin_products.js
@@ -7,10 +7,20 @@ const router = express.Router();
 
 // admin/products
 router.post('/products', (req, res) => {
-    // Hace falta cuando regresa 404
-    res.statusCode = 201;
-    let new_product = dataHandlerFile.createProduct(req.body);
-    res.send({'message': 'Producto cargado'});
+    let result = {};
+
+    // Detectar si el producto ya existe (con base en el uuid)
+    const uuid = req.body['uuid'];
+    const found = dataHandlerFile.getProductsById(uuid);
+    if(found != false) { // El producto ya existe
+        res.statusCode = 400;
+        result = {'message': `Producto con uuid ${uuid} ya existe`};
+    } else { // Si es false, entonces no encontró y se puede crear el producto
+        res.statusCode = 201;
+        let new_product = dataHandlerFile.createProduct(req.body);
+        result = {'message': 'Producto cargado con exitosamente'};
+    }
+    res.send(result);
 });
 
 // admin/products/:id
@@ -20,10 +30,10 @@ router.put('/products/:id', (req, res) => {
     const uuid = req.params.id;
     const found = dataHandlerFile.getProductsById(uuid);
 
-    if (!found) {
+    if (!found) { // Cuando el producto no se encontró
         res.statusCode = 404;
         result = {'message': 'Producto no encontrado'};
-    } else {
+    } else { // Cuando el producto sí se encontró
         res.statusCode = 200;
         new_data = req.body;
         dataHandlerFile.updateProduct(uuid, new_data);
