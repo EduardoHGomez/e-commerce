04f8442264d572051bf75478de66cd8f3d4d34b7
Eduardo <hurtado.josegomez@gmail.com>
2023-11-01 23:57:19 -0600

    SessionStorage function added Shopping cart

--- app/views/shopping_cart.js
+++ app/views/shopping_cart.js
@@ -2,6 +2,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("HTML Shopping cart Loaded");
 
-
     // Load cart
+    let container = document.querySelector('#productsContainer');
+
+
+
 });
+
+// Cargar desde sessionStorage
+function loadCart() {
+    console.log(sessionStorage);
+}
